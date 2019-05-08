fe21516e1a0f76fd5cd8308b9766b251ec07fdbc
name1 <minliqazqwe@gmail.com>
2019-05-08 13:20:00 -0700

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,10 +3,17 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
+import os
 
 MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')  
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server  
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
@@ -76,8 +83,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+    
 
     def tearDown(self):
         self.browser.quit()
